c427edb89976b06507e1c341a50ae16363061ad7
OBolsa <vitor-hug@hotmail.com>
2023-03-20 16:53:44 -0300

    Limpeza do código e dos sistemas

--- ProjetoT/Assets/Systems/Interactable/InteractablePointReferencer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InteractablePointReferencer : MonoBehaviour
-{
-    [SerializeField] private InteractionDisplayer m_Displayer;
-    public InteractionDisplayer Displayer => m_Displayer;
-}
