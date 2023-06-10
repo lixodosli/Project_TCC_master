3f1b5b02e3d5d81e8fb05bc2f31ecef7e0845875
OBolsa <vitor-hug@hotmail.com>
2023-06-10 19:02:16 -0300

    Conversas

---
+++ ProjetoT/Assets/Systems/DialogueSystem/Data/System/DialogueNode.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class DialogueNode : ScriptableObject
+{
+    public virtual void Execute() { }
+}
---
+++ ProjetoT/Assets/Systems/DialogueSystem/Data/System/NewConversation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Conversation", menuName = "Scriptable Objects/Dialogue System/NewConversation")]
+public class NewConversation : ScriptableObject
+{
+    [Header("Configs")]
+    [TextArea] public string ConversationContext;
+
+    [Header("Dialogue")]
+    [SerializeReference] public DialogueNode FirstDialogues;
+}
+
+public enum CheckageType
+{
+    ItemCheckage,
+    QuestCheckage,
+    TimeCheckage
+}
