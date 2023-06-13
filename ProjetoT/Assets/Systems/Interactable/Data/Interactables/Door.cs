67afbda503c630daa2bf7953d0cb1a83b89ea568
OBolsa <vitor-hug@hotmail.com>
2023-06-13 01:25:46 -0300

    Thats all folks

--- ProjetoT/Assets/Systems/Interactable/Data/Interactables/Door.cs
+++ ProjetoT/Assets/Systems/Interactable/Data/Interactables/Door.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Door : Interactable
 {
     [SerializeField] private TeleportConfig m_TeleportConfig;
+    public int TimePass;
 
     public override void DoInteraction()
     {
         TeleportManager.Instance.RaiseTeleport(m_TeleportConfig);
+        Messenger.Broadcast(TimeManager.AdvanceTimeString, TimePass);
     }
 }
